1963e9991a1066d490a608d34758b29f32cc3814
ttmfox <thien0972089317@gmail.com>
2024-10-02 17:15:58 +0700

    first commit

---
+++ helloword.java
@@ -0,0 +1,11 @@
+package hoidanit;
+
+public class helloword {
+
+	public static void main(String[] args) {
+		System.out.println("Hello word ");
+		System.out.println("Hello word ");
+		System.out.println("Hello word ");
+		System.out.println("Hello word ");
+	}
+}
